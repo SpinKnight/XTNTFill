0c395a98d3127f78368015a59da62d407a259657
SpinKnight <teemoflipper@gmail.com>
2021-03-25 17:28:27 +0100

    Renamed Modules & Fixed command logging when using alias from config

--- Plugin/src/dev/spinn/plugins/xtntfill/listener/CmdAliasesListener.java
+++ Plugin/src/dev/spinn/plugins/xtntfill/listener/CmdAliasesListener.java
@@ -1,7 +1,6 @@
 package dev.spinn.plugins.xtntfill.listener;
 
 import dev.spinn.plugins.xtntfill.XTNTFill;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -20,9 +19,8 @@ public class CmdAliasesListener implements Listener {
         int cmdLength = cmd.length() - 1; // -1 = slash
         for (String alias : plugin.getConfig().getStringList("cmd-aliases")) {
             if (!cmd.startsWith("/" + alias) || (cmdLength > alias.length() && cmd.charAt(alias.length() + 1) != ' ')) continue;
-            event.setCancelled(true);
             String args = cmd.substring(alias.length() + 1);
-            Bukkit.dispatchCommand(event.getPlayer(),"tntfill" + args);
+            event.setMessage("/tntfill" + args);
             break;
         }
     }
